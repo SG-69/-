9d36bbd0b14959b27248a91ec78b7b14c9a7db40
69 <121702240+SG-69@users.noreply.github.com>
2023-02-13 23:38:56 +0900

    Add files via upload

--- Project.Higurashi/datas/ProjectHigurashiAutoUpdater.lua
+++ Project.Higurashi/datas/ProjectHigurashiAutoUpdater.lua
@@ -10,8 +10,6 @@ local file_paths = {
     scriptevents = appdata_path .. "\\scripts\\Project.Higurashi\\datas\\HigurashiScriptEvents.lua",
     vehs = appdata_path .. "\\scripts\\Project.Higurashi\\datas\\HigurashiVehicles.lua",
     weapons = appdata_path .. "\\scripts\\Project.Higurashi\\datas\\HigurashiWeapons.lua",
-    --changelog = appdata_path .. "\\scripts\\Project.Higurashi\\Changelog.txt",
-    --version = appdata_path .. "\\scripts\\Project.Higurashi\\Version.txt",
     sep = appdata_path .. "\\cfg\\sep.cfg"
 }
 
@@ -24,8 +22,6 @@ local files = {
     scriptevents = [[https://raw.githubusercontent.com/SG-69/-/main/Project.Higurashi/datas/HigurashiScriptEvents.lua]],
     vehs = [[https://raw.githubusercontent.com/SG-69/-/main/Project.Higurashi/datas/HigurashiVehicles.lua]],
     weapons = [[https://raw.githubusercontent.com/SG-69/-/main/Project.Higurashi/datas/HigurashiWeapons.lua]],
-    --changelog = [[https://raw.githubusercontent.com/SG-69/-/main/Project.Higurashi/Changelog.txt]],
-    --version = [[https://raw.githubusercontent.com/SG-69/-/main/Project.Higurashi/Version.txt]],
     sep = [[https://raw.githubusercontent.com/SG-69/-/main/sep.cfg]]
 }
 
